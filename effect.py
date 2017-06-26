182c2839d70d1fc2d5596ac55ce723f1518d270f
Elllender <frimov2017@yandex.ru>
2017-06-26 23:39:23 +0400

    Update effect.py

--- effect.py
+++ effect.py
@@ -21,12 +21,12 @@ def effect(path,path_save):
 
   for x in range(0,size[0]):
     for y in range(0,size[1]):
-      color = img.getpixel((x,y)) 
+      color = img.getpixel((x,y))
       mas.append(color)
   for m in range(0,len(mas)):
-     k = randint(0,2)
+     k = randint(0,2) # Рандомное значение (чем меньше тем "шум" чаще) для чайников :)
      if k == 1:
-       mas[m] = mas[m-1]
+       mas[m] = mas[m-1] # Замена на предыдущий пиксель можно mas[-m+1] будет вверх.
      else:
        pass
   for x in range(0,size[0]):
